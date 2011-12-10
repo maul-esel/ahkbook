550b566ac707beaa64e5d01deee0c367e079f669
maul-esel <maul.esel@go4more.de>
2011-12-11 00:23:26 +0100

    separate build process in 2 scripts

--- build/zip.sh
+++ build/zip.sh
@@ -1,23 +1,2 @@
 #!/bin/sh
-
-# Location to save ebook
-OPATH=`pwd`
-
-# Custom dir to build in
-BPATH='/tmp/ahkbook_build'
-mkdir $BPATH
-cd $BPATH
-
-# clone from parent directory
-git clone $OPATH/.. ahkbook
-cd ahkbook
-
-jekyll --url '..'
-cd _site
-
-sed -i "s/href='\.\.\/css\/\([^\n]\+\)\.css'/href='css\/\1\.css'/g" index.html
-
-zip -r $OPATH/ahkbook.zip *
-
-cd $OPATH
-rm -R -f $BPATH
+zip -r $1/ahkbook.zip *
